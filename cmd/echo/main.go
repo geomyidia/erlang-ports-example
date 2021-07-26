62cd0d0588e5a7c5140c17d80eb46f0a624bcf92
Duncan McGreggor <oubiwann@gmail.com>
2021-07-25 19:39:16 -0500

    Re-orged standard project layout.

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"github.com/geomyidia/erlang-port-examples/echo"
-	"github.com/geomyidia/erlang-port-examples/port"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/geomyidia/erlang-port-examples/internal/app"
+	"github.com/geomyidia/erlang-port-examples/pkg/echo"
+	"github.com/geomyidia/erlang-port-examples/pkg/port"
 )
 
 func main() {
-	port.SetupLogging()
-	port.SetupRandom()
+	log.Info("Starting up Go Port example ...")
+	app.SetupLogging()
+	app.SetupRandom()
 	port.ProcessPortMessages(echo.ProcessEchoCommand)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,4 +1,4 @@
-package port
+package app
 
 import (
 	"math/rand"
